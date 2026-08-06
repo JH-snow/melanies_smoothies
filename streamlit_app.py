e60a0946db0d632c1b2c636580dec6c64e066e6e
JH-snow <jh.choi2327@hnix.co.kr>
2026-08-06 14:32:34 +0900

    Fix typo in connection method for Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-conn = st.connetion("snowflake")
+conn = st.connection("snowflake")
 session = conn.session()
 
 # Write directly to the app
